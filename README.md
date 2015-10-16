51d6e1c6af17fc0d84691eed93b7097fd632eae2
BurritoLove <dogwithaface@gmail.com>
2015-10-16 01:41:01 -0500

    Just added the main project files.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # J-CAT
+install android sdk.. load sdk manager and download the 
+													23.0.1 sdk build tools
+													20 on android api
+eclipse
+	get the gradle plugin
+netbeans do the same as eclipse.
+
+to learn github go here
+https://www.youtube.com/watch?v=0fKg7e37bQE
---
+++ android/src/jcat/assembly/sim/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jcat.assembly.sim.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jcat.assembly.sim.AssemblySim;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AssemblySim(), config);
+	}
+}
